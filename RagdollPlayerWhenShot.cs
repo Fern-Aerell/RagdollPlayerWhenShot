c57a5fb2998b7ec89d30f1b6ca537af338af48a5
Fern-Aerell <fernaerell2020@gmail.com>
2024-09-28 12:14:32 +0700

    Update 1.3.30.0, Fix Bugs, New Features

--- RagdollPlayerWhenShot.cs
+++ RagdollPlayerWhenShot.cs
@@ -1,62 +1,57 @@
 ﻿using System;
 using GTA;
-using GTA.Math;
-using GTA.Native;
+using GTA.UI;
 
 public class RagdollPlayerWhenShot : Script
 {
-    private readonly string version = "1.0.0";
+    private readonly string version = "1.3.30.0";
     private int lastHealthAndArmor;
     private readonly Random random = new Random();
+    private bool init = false;
 
     public RagdollPlayerWhenShot()
     {
         Tick += OnTick;
         lastHealthAndArmor = GetPlayerHealthAndArmorValue();
-        ShowWatermark();
+    }
+
+    private void Initialize()
+    {
+        if (!init)
+        {
+            Screen.ShowHelpText($"RagdollPlayerWhenShot Script V{version}, By Fern Aerell.", 10000, true, false);
+            Wait(10000);
+            Screen.ShowHelpText($"Press ~INPUT_JUMP~ to wake up if you're stuck in ragdoll.", 10000, true, false);
+            init = true;
+        }
     }
 
     private void OnTick(object sender, EventArgs e)
     {
+        Initialize();
+
         if (Game.Player.Character.IsAlive)
         {
-            int currentHealthAndArmor = GetPlayerHealthAndArmorValue();
-
-            if (currentHealthAndArmor < lastHealthAndArmor && IsPlayerHitByBullet())
+            foreach (var ped in World.GetAllPeds())
             {
-                int minTime = random.Next(500, 1000);
-                Game.Player.Character.SetToRagdoll(minTime, minTime * 5, RagdollType.Balance, true);
+                if (ped.IsShooting && ped.CombatTarget != null && ped.CombatTarget.IsPlayer)
+                {
+                    int currentHealthAndArmor = GetPlayerHealthAndArmorValue();
+                    if(currentHealthAndArmor < lastHealthAndArmor && !(Game.IsControlPressed(Control.Jump) && Game.Player.Character.IsRunningRagdollTask))
+                    {
+                        int minTime = random.Next(500, 1000);
+                        float healthPercentage = (float)Game.Player.Character.Health / Game.Player.Character.MaxHealth;
+                        int maxTime = (int)(minTime * (1 + (1 - healthPercentage)));
+                        Game.Player.Character.SetToRagdoll(minTime, maxTime, RagdollType.Balance, true);
+                    }
+                    lastHealthAndArmor = currentHealthAndArmor;
+                }
             }
-
-            lastHealthAndArmor = currentHealthAndArmor;
         }
-    }
-
-    private bool IsPlayerHitByBullet()
-    {
-        foreach (var ped in World.GetAllPeds())
-        {
-            if (ped.IsShooting && IsBulletHittingPlayer(ped.Position + new Vector3(0, 0, 1)))
-                return true;
-        }
-        return false;
-    }
-
-    private bool IsBulletHittingPlayer(Vector3 bulletOrigin)
-    {
-        var result = World.Raycast(bulletOrigin, Game.Player.Character.Position, IntersectFlags.PedCapsules);
-        return result.DidHit && result.HitEntity == Game.Player.Character;
     }
 
     private int GetPlayerHealthAndArmorValue()
     {
         return Game.Player.Character.Health + Game.Player.Character.Armor;
-    }
-
-    private void ShowWatermark()
-    {
-        Function.Call(Hash.BEGIN_TEXT_COMMAND_DISPLAY_HELP, "STRING");
-        Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, $"RagdollPlayerWhenShot Script V{version} By Fern Aerell.");
-        Function.Call(Hash.END_TEXT_COMMAND_DISPLAY_HELP, 0, false, true, -1);
     }
 }
